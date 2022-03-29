446b7a7c65e7fe785029fafff751d66e2e91fca2
Davie <webdev.inkshared@gmail.com>
2022-03-30 05:23:19 +0800

    added reviews

---
+++ src/components/parallax-bottom.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import {Link} from 'gatsby'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+
+const ParallaxBottom = () => {
+
+
+    const settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        initialSlide: 0,
+        adaptiveHeight: true,
+        infinite: true,
+    }
+
+    return (
+        <>
+            <section className="parallax-bottom">
+           
+                <div className="flex-container">
+                    
+                <Slider {...settings}> 
+                        <div className="parallax-item">
+                            <h1>
+                            "True knowledge comes from observing life in a way nobody ever has."
+                                <span className="italic-tag span-verse">
+                                Heather Brooks
+                                </span>
+                            </h1>
+                        </div>
+                        <div className="parallax-item">
+                             <h1>
+                             "This stream-of-consciousness fantasy calls upon the reader to pay careful attention to the ultimate goals of each of the numerous characters."
+                               <div className="a-button"><a href="https://www.theusreview.com/reviews-1/Seven-Islands-of-the-Fog-by-Thomas-Terraforte.html#.YkNvtudByUk" target="_blank">Read more</a></div> 
+                                <span className="italic-tag span-verse">
+                                The US Review of Books
+                                </span>
+                            </h1>
+                           
+                        </div>
+                </Slider>
+                </div>
+            </section>
+            
+
+        </>
+    )
+}
+
+export default ParallaxBottom
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Nav from "../components/nav"
 import Banner from "../components/banner"
 import UpperMain from "../components/upper-main"
 import MidMain from "../components/mid-main"
+import ParallaxBottom from "../components/parallax-bottom"
 import Newsletter from "../components/newsletter"
 
 const IndexPage = (props) => (
@@ -13,6 +14,7 @@ const IndexPage = (props) => (
       <Nav pathExt={props.path}/>
       <Banner/> 
       <UpperMain/> 
+      <ParallaxBottom/>
       <MidMain/>
       <Newsletter/> 
   </Layout>
